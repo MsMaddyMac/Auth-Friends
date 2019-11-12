d672cc334b5d6772eaac546c63e4650242360f69
Maddy <mmcintosh2k14@gmail.com>
2019-11-12 18:24:51 -0500

    adds boilerplate to reducer index.js

--- friends/src/App.js
+++ friends/src/App.js
@@ -1,14 +1,27 @@
 import React from 'react';
 import './App.css';
 // Redux imports
+import { applyMiddleware, createStore } from 'redux';
+// react-redux imports
+import { Provider } from 'react-redux';
+import thunk from 'redux-thunk';
+//component imports
+
+
+const store = createStore(
+  reducer, 
+  (applyMiddleware(thunk))
+);
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <h1>Friends List Coming Soon!</h1>
-      </header>
-    </div>
+    <Provider store={store}>
+      <div className="App">
+        <header className="App-header">
+          <h1>Friends List Coming Soon!</h1>
+        </header>
+      </div>
+    </Provider>
   );
 }
 
---
+++ friends/src/reducers/index.js
@@ -0,0 +1,12 @@
+const initialState = {
+    friends: [],
+    error: null,
+    isFetching: false
+};
+
+function reducer(state = initialState, action) {
+    switch(action.type) {
+        default:
+            return state;
+        }
+}
