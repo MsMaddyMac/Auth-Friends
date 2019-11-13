ec658f669ea3406c62da09f35517e775976380a4
Maddy <mmcintosh2k14@gmail.com>
2019-11-13 12:05:31 -0500

    adds switch cases to reducer-index.js

--- friends/src/reducers/index.js
+++ friends/src/reducers/index.js
@@ -1,3 +1,9 @@
+import { 
+    GET_FRIENDS_LOADING, GET_FRIENDS_SUCCESS,
+    GET_FRIENDS_FAILURE, POST_NEW_FRIEND_POSTING,
+    POST_NEW_FRIEND_SUCCESS, POST_NEW_FRIEND_FAILURE
+} from '../actions';
+
 const initialState = {
     friends: [],
     error: null,
@@ -6,6 +12,47 @@ const initialState = {
 
 function reducer(state = initialState, action) {
     switch(action.type) {
+        case GET_FRIENDS_LOADING:
+            return {
+                ...state,
+                isFetching: true,
+                error: null
+            };
+        case GET_FRIENDS_SUCCESS:
+            return {
+                ...state,
+                friends: action.payload,
+                isFetching: false,
+                error: null
+            };
+        case GET_FRIENDS_FAILURE:
+            return {
+                ...state,
+                smurfs: [],
+                isFetching: false,
+                error: action.payload
+            };
+        case POST_NEW_FRIEND_POSTING:
+            return {
+                ...state,
+                isFetching: true,
+                errors: null,
+                friends: []
+            };
+        case POST_NEW_FRIEND_SUCCESS:
+            return {
+                ...state,
+                isFetching: false,
+                errors: null,
+                friends: action.payload
+            };
+        case POST_NEW_FRIEND_FAILURE:
+            return {
+                ...state,
+                isFetching: false,
+                errors: action.payload,
+                friends: []
+            };
         default:
             return state;
         }
