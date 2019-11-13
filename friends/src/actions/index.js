08d7aa9be02a513f089b6b4dd5d4df8ff8284497
Maddy <mmcintosh2k14@gmail.com>
2019-11-13 11:54:02 -0500

    adds actions to actions - index.js

---
+++ friends/src/actions/index.js
@@ -0,0 +1,39 @@
+import axiosWithAuth from '../utils/axiosWithAuth';
+
+export const GET_FRIENDS_LOADING = 'GET_FRIENDS_LOADING';
+export const GET_FRIENDS_SUCCESS = 'GET_FRIENDS_SUCCESS';
+export const GET_FRIENDS_FAILURE = 'GET_FRIENDS_FAILURE';
+
+export const friendsLoading = () => ({ type: GET_FRIENDS_LOADING });
+export const friendsLoadingSuccess = () => ({ type: GET_FRIENDS_SUCCESS, payload: data });
+export const friendsLoadingFailure = () => ({ type: GET_FRIENDS_FAILURE, payload: error });
+
+export function getFriends() {
+    return function(dispatch) {
+        return axiosWithAuth()
+        .get('/friends')
+        .then(res => dispatch(friendsLoading(res.data)))
+        .catch(err => dispatch(friendsLoadingFailure(), err)
+        );
+    };
+}
+
+export const POST_NEW_FRIEND_POSTING = 'POST_NEW_FRIEND_POSTING';
+export const POST_NEW_FRIEND_SUCCESS = 'POST_NEW_FRIEND_SUCCESS';
+export const POST_NEW_FRIEND_FAILURE = 'POST_NEW_FRIEND_FAILURE';
+
+export const newFriendPosting = () => ({ type: POST_NEW_FRIEND_POSTING })
+export const newFriendSuccess = () => ({ type: POST_NEW_FRIEND_SUCCESS })
+export const newFriendFailure = () => ({ type: POST_NEW_FRIEND_FAILURE })
+
+export function postFriend(friend) {
+    return function(dispatch) {
+        dispatch(newFriendPosting());
+        return axiosWithAuth()
+        .post('/friends', friend)
+        .then(res => {console.log(res.data)
+            dispatch(newFriendSuccess())})
+        .catch(err => dispatch(newFriendFailure(), err)
+        );
+    }
+}
