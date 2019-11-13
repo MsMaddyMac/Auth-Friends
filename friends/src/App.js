13e76db403bdc3d536171a0ca181da1463b36a4b
Maddy <mmcintosh2k14@gmail.com>
2019-11-12 20:36:17 -0500

    adds refactored App.js and index.js components

--- friends/src/App.js
+++ friends/src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import './App.css';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 //component imports
 import FriendsForm from './components/FriendsForm';
 // import FriendsList from './components/FriendsList';
@@ -9,13 +10,15 @@ import FriendsForm from './components/FriendsForm';
 
 function App() {
   return (
-      <div className="App">
-        <header className="App-header">
-          <h1>Friends List Coming Soon!</h1>
-        </header>
-        <FriendsForm />
-        {/* <FriendsList /> */}
-      </div>
+      <Router>
+        <div className="App">
+          <header className="App-header">
+            <h1>Friends List Coming Soon!</h1>
+          </header>
+          <FriendsForm />
+          {/* <FriendsList /> */}
+        </div>
+      </Router>
   );
 }
 
