f000c4aa4a9c65e4bfcf26e2ec2a891265b4d269
Maddy <mmcintosh2k14@gmail.com>
2019-11-12 20:34:55 -0500

    adds renamed register.js to login.js

--- friends/src/App.js
+++ friends/src/App.js
@@ -1,24 +1,14 @@
 import React from 'react';
 import './App.css';
-// Redux imports
-import { applyMiddleware, createStore } from 'redux';
-// react-redux imports
-import { Provider } from 'react-redux';
-import thunk from 'redux-thunk';
 //component imports
-import reducer from './reducers';
 import FriendsForm from './components/FriendsForm';
 // import FriendsList from './components/FriendsList';
 
 
-const store = createStore(
-  reducer, 
-  (applyMiddleware(thunk))
-);
+
 
 function App() {
   return (
-    <Provider store={store}>
       <div className="App">
         <header className="App-header">
           <h1>Friends List Coming Soon!</h1>
@@ -26,7 +16,6 @@ function App() {
         <FriendsForm />
         {/* <FriendsList /> */}
       </div>
-    </Provider>
   );
 }
 
--- friends/src/index.js
+++ friends/src/index.js
@@ -2,7 +2,22 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+// Redux imports
+import { applyMiddleware, createStore } from 'redux';
+// react-redux imports
+import { Provider } from 'react-redux';
+import thunk from 'redux-thunk';
+//component imports
+import reducer from './reducers';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const store = createStore(
+    reducer, 
+    (applyMiddleware(thunk))
+  );
+
+ReactDOM.render(
+    <Provider store={store}>
+        <App />
+    </Provider>, document.getElementById('root'));
 
 
